0dbe7e93a39b92fe33a887d25dfd1081ec4f3d6d
Lucas Tostée <lucas.tostee@gmail.com>
2019-02-24 18:32:28 +0100

    Update component

--- src/view/routes/CharacterInfo.js
+++ src/view/routes/CharacterInfo.js
@@ -2,84 +2,80 @@
  * Import
  */
 import React from 'react';
-import {getData} from "../model/functions";
+import {NavLink} from "react-router-dom";
+import AppContext from "../../model/store/app-context";
 
 /**
  * CharacterInfo: Hold more infos about a character
  */
 export default class CharacterInfo extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {}
+    static contextType = AppContext;
+
+    componentWillUnmount() {
+        delete this.context.character;
     }
-    componentWillMount = async () => {
-        let characterData = await getData(`https://gateway.marvel.com/v1/public/characters/${this.props.match.params.id}?apikey=f5d9657d8bb68f805b1ec30fe13cf70d`);
-        this.setState({data:characterData.data.results[0]});
-    }
+
     render() {
-        if (this.state.data) {
-            return (
-                <React.Fragment>
-                    <header className="container mb-3">
-                        <div className="row">
-                            <div className="col-12">
-                                <h1 className="text-center">{this.state.data.name}</h1>
-                            </div>
-                        </div>
-                    </header>
-                    <section className="container description">
-                        <div className="row">
-                            <div className="col-12">
-                                <p className="lead">{this.state.data.description}</p>
-                                <p className="small"><strong>Resource API</strong>: {this.state.data.resourceURI}</p>
-                            </div>
-                        </div>
-                    </section>
-                    <section className="container stories mb-3">
-                        <div className="row">
-                            <div className="col-12">
-                                <h2>Appears in:</h2>
-                                <ul className="list-group">
-                                    {
-                                        this.state.data.stories.items.map(el => {
-                                            return <li
-                                                key={this.state.data.id--}
-                                                className="list-group-item small">{el.name}</li>
-                                        })
-                                    }
-                                </ul>
-                            </div>
-                        </div>
-                    </section>
-                    <section className="container more mb-3">
-                        <div className="row">
-                            <div className="col-12">
-                            <h2>See more:</h2>
-                                <ul className="list-group">
-                                    {
-                                        this.state.data.urls.map(el => {
-                                            return <li 
-                                            key={this.state.data.id++} 
-                                            className="list-group-item small">{el.type}: 
-                                                <a href={el.url}>{el.url}</a>
-                                            </li>
-                                        })
-                                    }
-                                </ul>
-                            </div>
-                        </div>
-                    </section>
-                    <section className="container">
-                        <div className="row">
-                            <div className="col-12">
-                                <button className="btn btn-success"><a href="/" className="is__link__info">Home</a></button>
-                            </div>
-                        </div>
-                    </section>
-                </React.Fragment>
-            );
-        } else {
-            return null;
-        }
+        this.context.marvelData.map(el => parseInt(this.props.match.params.id) === el.id ? this.context.character = el : null);
+        return (<React.Fragment>
+            <header className="container mb-3">
+            <div className="row">
+                    <div className="col-12">
+                        <h1 className="text-center">{this.context.character.name}</h1>
+                    </div>
+                </div>
+            </header>
+            <section className="container description">
+                <div className="row">
+                    <div className="col-12">
+                        <p className="lead">{this.context.character.description}</p>
+                        <p className="small"><strong>Resource API</strong>: {this.context.character.resourceURI}</p>
+                    </div>
+                </div>
+            </section>
+            <section className="container stories mb-3">
+                <div className="row">
+                    <div className="col-12">
+                        <h2>Appears in:</h2>
+                        <ul className="list-group">
+                            {
+                                this.context.character.stories.items.map(el => {
+                                    return <li
+                                        key={this.context.character.id--}
+                                        className="list-group-item small">{el.name}</li>
+                                })
+                            }
+                        </ul>
+                    </div>
+                </div>
+            </section>
+            <section className="container more mb-3">
+                <div className="row">
+                    <div className="col-12">
+                        <h2>See more:</h2>
+                        <ul className="list-group">
+                            {
+                                this.context.character.urls.map(el => {
+                                    return <li
+                                        key={this.context.character.id++}
+                                        className="list-group-item small">{el.type}:
+                                <a href={el.url}>{el.url}</a>
+                                    </li>
+                                })
+                            }
+                        </ul>
+                    </div>
+                </div>
+            </section>
+            <section className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <button className="btn btn-success">
+                            <NavLink to="/" className="is__link__info">Home</NavLink>
+                        </button>
+                    </div>
+                </div>
+            </section>
+        </React.Fragment>);
     }
 }
