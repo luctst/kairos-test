2fc68278aadfed638e546b88bda96fb5be92b53f
Lucas Tostée <lucas.tostee@gmail.com>
2019-02-24 18:32:46 +0100

    Update component

--- src/view/app.js
+++ src/view/app.js
@@ -1,75 +1,40 @@
 /**
- * Import
+ * Import, Variables
  */
 import React from "react";
-import {getData} from "../model/functions";
 import AppContext from "../model/store/app-context";
-import Cards from "./components/Cards";
-import {CharactersBtn} from "./components/CharactersBtn";
+import Cards from "./components/container/Cards";
+import {CharactersBtn} from "./components/container/CharactersBtn";
 import {CharactersLoaded} from "./components/CharactersLoaded";
-import Header from "./components/Header";
-import Loader from "./components/Loader";
-
-/**
- * Variables
- */
-const marvelApiKey = `apikey=f5d9657d8bb68f805b1ec30fe13cf70d`;
-const marvelUrlApi = `https://gateway.marvel.com/v1/public`;
 
 /**
  * App: HOC component holds all the component's of our app.
  */
 export default class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state =  {};
-        fetch(`${marvelUrlApi}/characters?limit=10&${marvelApiKey}`)
-            .then(data => data.json())
-            .then(dataParsed => this.state.marvelData =dataParsed.data.results);
+    static contextType = AppContext;
+
+    state = {
+        cl: 10
     }
 
     getInputValue = event => {
         if (event.target.value !== "") {
-            this.setState({
-                charactersLimit: parseInt(event.target.value),
-                dataLoading: true
-            });
-        } else if (event.target.value === "") {
-            this.setState({ charactersLimit: 10 });
+            this.setState({cl: parseInt(event.target.value)});
+            this.context.cl = parseInt(event.target.value);
+            return this.context.pageNumber = event.target.value / 5;
+        } else {
+            this.context.cl = 10;
+            return this.setState({cl: 10});
         }
     }
 
-    // componentDidUpdate = async () => {
-    //     if (this.state.charactersLimit !== this.state.marvelData.length) {
-    //         let resData = await getData(`${marvelUrlApi}/characters?limit=${this.state.charactersLimit}&${marvelApiKey}`);
-    //         this.setState({marvelData:resData.data.results, dataLoading: false });
-    //     }
-    // }
-
     render() {
         return (
-            <AppContext.Provider value={{data: this.state.marvelData}}>
-                {(this.state.loaderActive) ? <Loader />:
-                <React.Fragment>
-                    <Header/>
-                    <CharactersBtn
-                        getValue={this.getInputValue}
-                        display={this.state.dataLoading} />
-                    <CharactersLoaded number={this.state.marvelData.length} />
-                    <main className="container mt-3 mb-3">
-                        {
-                            this.state.marvelData.map(el => {
-                                return <Cards
-                                    id={el.id}
-                                    name={el.name}
-                                    tab={el.series.items}
-                                    key={el.id}
-                                    image={`${el.thumbnail.path}.${el.thumbnail.extension}`} />
-                            })
-                        }
-                    </main>
-                </React.Fragment>}
-            </AppContext.Provider>
+            <React.Fragment>
+                <CharactersBtn handleInputValue={this.getInputValue}/>
+                <CharactersLoaded/>
+                <Cards/>
+            </React.Fragment>
         );
     }
 }
